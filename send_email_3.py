af19d34aefc5d5fda503d37dcd90caeb4ab96980
Thomas Langan <tom.langan@comcast.net>
2024-08-18 20:30:27 -0400

    Import email module into ping module

--- send_email_3.py
+++ send_email_3.py
@@ -15,11 +15,12 @@ From Ping Project without alteration from Email Project: Unfortunately this doma
 user = '8c14705c6103d7'
 password = 'd246138e6950f5'
 
-def sendEmail():
+def sendEmail(domain):
     with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
         server.starttls()
         server.login(user, password)
         server.sendmail(sender, receiver, message)
         print("mail successfully sent")
 
-sendEmail()
+#domain = "googletommy.com"
+#sendEmail(domain)
